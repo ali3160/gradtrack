a2328ff57d3e9ef241f7f85ca0dd7d0e4f545be2
ali3160 <a.e.naas.2000@gmail.com>
2026-06-09 15:45:00 +0100

    Add Swagger JWT bearer authenticatioin

--- src/main/java/com/gradtrack/config/OpenApiConfig.java
+++ src/main/java/com/gradtrack/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package com.gradtrack.config;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +14,22 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI gradTrackOpenAPI() {
+
+        String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("GradTrack API")
                         .version("1.0.0")
-                        .description("REST API for tracking graduate job applications, interviews, tasks, deadlines and activity history."));
+                        .description("REST API for tracking graduate job applications, interviews, tasks, deadlines and activity history."))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
+
 }
